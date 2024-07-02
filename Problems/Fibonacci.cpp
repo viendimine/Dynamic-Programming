8155646c936c1abe66a5c53c307746acbdb97f22
Vishap Raj <126682925+viendimine@users.noreply.github.com>
2024-07-02 23:08:51 +0530

    Update Fibonacci.cpp

--- Problems/Fibonacci.cpp
+++ Problems/Fibonacci.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int Fibonacci(int a)// with recursion
+int Fibonacci(int a)//Using recursion
 {
     if(a==0)
         return 0;
@@ -11,7 +11,7 @@ int Fibonacci(int a)// with recursion
         return Fibonacci(a-1)+Fibonacci(a-2);
 }
 
-int fibonacci(int a)
+int fibonacci(int a)//Using Memoization
 {
     vector<int>dp(a+1,-1);
     if(a<=1)
@@ -22,10 +22,22 @@ int fibonacci(int a)
         return dp[a]=fibonacci(a-1)+fibonacci(a-2);
 }
 
+int Fib(int a)//Using Tabulation
+{
+   vector<int>dp(a+1);
+   dp[0]=0,dp[1]=1;
+   for(int i=2;i<=a;i++)
+   {
+    dp[i]=dp[i-1]+dp[i-2];
+   }
+   return dp[a];
+
+}
 int main()
 {
     int n;
     cin>>n;
     cout<<fibonacci(n)<<endl;
     cout<<Fibonacci(n)<<endl;
-}
+    cout<<Fib(n);
+}`
